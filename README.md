9170c58e7237116b4d617c9bf2888ca13b6c9e6e
Mike Kaliman <kaliman.mike@gmail.com>
2022-06-08 22:00:32 -0400

    Abandon the codegen

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Lox interpreter as part of the Crafting Interpreters book whose source code can be found at <https://github.com/munificent/craftinginterpreters>
 
+<https://github.com/rodaine/rlox> was useful as a reference for translating the Java visitor pattern to Rust
+
 Current chapter: 5
 
 the main binary can parse a script or work as a repl
--- src/expr.rs
+++ src/expr.rs
@@ -1,9 +1,32 @@
 use crate::token::LiteralType;
 use crate::token::Token;
 
+#[derive(Debug)]
 pub enum Expr {
-    Binary(Box<Expr>, Token, Box<Expr>), // left, operator, right
-    Grouping(Box<Expr>),                 // expression
-    Literal(LiteralType),                // whatever literal value except None i guess
-    Unary(Token, Box<Expr>),             // operator, right
+    /// Binary   : Expr left, Token operator, Expr right
+    Binary(Box<Expr>, Token, Box<Expr>),
+    /// Grouping : Expr expression
+    Grouping(Box<Expr>),
+    /// Literal  : Object value
+    Literal(LiteralType),
+    /// Unary    : Token operator, Expr right
+    Unary(Token, Box<Expr>),
+}
+
+pub trait Visitor<T> {
+    fn visit_binary(&mut self, lhs: Box<Expr>, op: Token, rhs: Box<Expr>) -> T;
+    fn visit_grouping(&mut self, expr: Box<Expr>) -> T;
+    fn visit_literal(&mut self, value: LiteralType) -> T;
+    fn visit_unary(&mut self, op: Token, right: Box<Expr>) -> T;
+}
+
+impl Expr {
+    pub fn accept<T>(&self, visitor: &mut dyn Visitor<T>) -> T {
+        match &self {
+            Expr::Binary(lhs, op, rhs) => todo!(),
+            Expr::Grouping(expr) => todo!(),
+            Expr::Literal(lit) => todo!(),
+            Expr::Unary(op, expr) => todo!(),
+        }
+    }
 }
