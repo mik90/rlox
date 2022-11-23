eaa55d0f516c9dd737943c36eda268cde6fded2e
Mike Kaliman <kaliman.mike@gmail.com>
2022-11-22 19:25:05 -0500

    working on 16.2

--- virtual-machine/src/compiler.rs
+++ virtual-machine/src/compiler.rs
@@ -1,3 +1,28 @@
+use crate::scanner::{Scanner, TokenKind};
+
 pub fn compile(source: &str) {
+    let mut scanner = Scanner::new(source);
+    let mut line = 0;
+    loop {
+        let token = scanner.scan_token();
+        if token.line != line || line == 0 {
+            println!("line:{:>4} ", token.line);
+            line = token.line;
+        } else {
+            println!("        | ");
+        }
+        let text: String = token
+            .start
+            .clone()
+            .into_iter()
+            .take(token.length)
+            .map(|(_, c)| c)
+            .collect();
+        println!("{:?} {}", token.kind, text);
+
+        if let TokenKind::Eof = token.kind {
+            break;
+        }
+    }
     todo!("compile({})", source)
 }
--- virtual-machine/src/scanner.rs
+++ virtual-machine/src/scanner.rs
@@ -7,6 +7,19 @@ pub struct Scanner<'a> {
     line: usize,
 }
 
+#[derive(PartialEq, Debug)]
+pub enum TokenKind {
+    Eof,
+    Other,
+}
+
+pub struct Token<'a> {
+    pub line: usize,
+    pub start: Enumerate<Chars<'a>>,
+    pub length: usize,
+    pub kind: TokenKind,
+}
+
 impl<'a> Scanner<'a> {
     pub fn new(source: &'a str) -> Self {
         Self {
@@ -14,5 +27,8 @@ impl<'a> Scanner<'a> {
             current: source.chars().into_iter().enumerate(),
             line: 1,
         }
+    }
+    pub fn scan_token(&mut self) -> Token {
+        todo!()
     }
 }
